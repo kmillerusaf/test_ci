8c5160cf34373d643d6bdfbbf04fd394c47c4507
Keith Miller <20777378+kmillerusaf@users.noreply.github.com>
2021-04-10 23:43:47 +0000

    Added test for multiply function

--- test_calculator.py
+++ test_calculator.py
@@ -11,3 +11,6 @@ class TestCalculator:
 
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
+
+    def test_multiplication(self):
+        assert 15 == calculator.multiply(5, 3)
